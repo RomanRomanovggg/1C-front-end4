3bdfef01fa0f00e429533ef3a709dc6bddd10281
RomanRomanovggg <32431878+RomanRomanovggg@users.noreply.github.com>
2017-10-01 15:33:25 +0300

    asd
    asd

---
+++ script.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("sanbox"),
+	context = canvas.getContext("2d");
+	square, circle;
+	square = new Path2D();
+	square.moveTo (50,50);
+	square.lineTo (250,50);
+	square.lineTo (250,250);
+	context.stroke(squar);
+	
